90de0272ecde99cc46b37945cdb5dc6052dbe34a
Muhammad Asif <asif88383@gmail.com>
2024-11-26 16:48:07 +0500

    add initial configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+// import About from './components/About'
+// import Hero from './components/Hero'
+// import NavBar from './components/Navbar'
+// import Features from './components/Features'
+// import Story from './components/Story'
+// import Contact from './components/Contact'
+// import Footer from './components/Footer'
 
 function App() {
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      {/* <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer /> */}
+    </main>
   )
 }
 
