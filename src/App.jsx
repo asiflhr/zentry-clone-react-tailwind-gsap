89483a7cbbb26a1f94aeabd0d11ce2b134b8d81d
Muhammad Asif <asif88383@gmail.com>
2025-02-26 16:28:58 +0500

    Added story section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from './components/About'
 import Hero from './components/Hero'
 import NavBar from './components/Navbar'
 import Features from './components/Features'
-// import Story from './components/Story'
+import Story from './components/Story'
 // import Contact from './components/Contact'
 // import Footer from './components/Footer'
 
@@ -13,8 +13,8 @@ function App() {
       <Hero />
       <About />
       <Features />
-      {/* <Story />
-      <Contact />
+      <Story />
+      {/* <Contact />
       <Footer /> */}
     </main>
   )
